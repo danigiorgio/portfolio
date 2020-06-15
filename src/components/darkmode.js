783b8e09cabf4e007f32abc6bbc7c91d28257ccd
voirs <danielagiorgioe@gmail.com>
2020-06-14 21:44:56 -0300

    fix dark mode icon, remove some global colors

--- src/components/darkmode.js
+++ src/components/darkmode.js
@@ -1,45 +1,31 @@
 import React from 'react'
-import useDarkMode from 'use-dark-mode'
 import styled from 'styled-components'
-import { IoMdSunny, IoIosMoon } from 'react-icons/io'
+import useDarkMode from 'use-dark-mode'
+import { FiMoon as Moon, FiSun as Sun } from 'react-icons/fi'
 
 const ToggleButton = styled.button`
   background: none;
   border: none;
   cursor: pointer;
   font-size: 1.5em;
-  transition: all 0.15s ease;
   overflow: hidden;
-  border-radius: 0.25rem;
   svg {
-    &:hover {
-      color: #747fe0;
-    }
+    color: #747fe0;
   }
-`
-
-const SunIcon = styled(IoMdSunny)`
-  color: #fff;
-`
-
-const MoonIcon = styled(IoIosMoon)`
-  color: #333;
 `
 
 const DarkModeToggle = () => {
   const darkMode = useDarkMode(false)
-  const a11yInfo = darkMode.value ? 'Use light theme' : 'Use dark theme'
+  const a11yInfo = darkMode.value ? 'Toggle light mode' : 'Toggle dark mode'
 
   return (
     <ToggleButton
       aria-label={a11yInfo}
       title={a11yInfo}
       onClick={darkMode.toggle}
-      type="button"
     >
-      {darkMode.value ? <SunIcon /> : <MoonIcon />}
+      {darkMode.value ? <Sun /> : <Moon />}
     </ToggleButton>
   )
 }
-
 export default DarkModeToggle
