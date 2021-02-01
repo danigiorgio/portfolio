316a2213bc320fe3578252293533f5898918732e
voirs <danielagiorgioe@gmail.com>
2021-02-01 20:52:47 -0300

    modify 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
+import SEO from '../components/seo';
+import styled from 'styled-components';
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 const NotFoundPage = () => (
-  <Layout>
+  <Wrapper>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+    <p>
+      You just hit a route that doesn't exist! Go back <a href="/">home.</a>
+    </p>
+  </Wrapper>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
