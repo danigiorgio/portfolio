a582850e0a36657752a424ab97ee5d4483ebb338
voirs <danielagiorgioe@gmail.com>
2021-02-11 00:30:35 -0300

    add .github and .editorconfig, modify prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
